03541d0e618bf2e890602d2b92fdfccd48f5c5c5
yasser taha <yasser1119992017@gmail.com>
2024-09-13 19:20:05 +0200

    create  four components{header,students,studentsdetails,errors, set config for route and down load bootstrap and add it to anguler.json}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentsComponent } from './components/students/students.component';
+import { ErrorsComponent } from './components/errors/errors.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterOutlet, StudentsComponent, ErrorsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StudentsComponent } from './components/students/students.component';
+import { StudentsDetailsComponent } from './components/students-details/students-details.component';
+import { ErrorsComponent } from './components/errors/errors.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:StudentsComponent}, 
+    {path:"students",component:StudentsComponent},
+    {path:"students/5",component:StudentsDetailsComponent},
+    {path:"**",component:ErrorsComponent},
+];
