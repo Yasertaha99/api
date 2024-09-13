d2f68e8c0850345f71909c1c60a0f56c84e0bc58
yasser taha <yasser1119992017@gmail.com>
2024-09-13 22:07:20 +0200

    show data one student and handle error page

--- src/app/components/students-details/students-details.component.ts
+++ src/app/components/students-details/students-details.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-students-details',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,RouterLink],
+  providers:[UsersService],
   templateUrl: './students-details.component.html',
   styleUrl: './students-details.component.css'
 })
-export class StudentsDetailsComponent {
+export class StudentsDetailsComponent implements OnInit{
+  ID:any; oneStudent:any;
+ constructor(private myService:UsersService,myActivate:ActivatedRoute){ 
+  this.ID=myActivate.snapshot.params['id'];
+  // console.log(this.ID);
+ }
+  ngOnInit(): void {
+    this.myService.getAllStudentById(this.ID).subscribe({
+      next:(data)=>{
+       this.oneStudent=data;
+      },
+      error:()=>{
 
+      }
+
+    });
+  }
 }
